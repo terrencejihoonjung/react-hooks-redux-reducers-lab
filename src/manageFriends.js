41ef3f3d22428769a503fb0636da540878e0bd56
terrencejung <terrencejung928@gmail.com>
2022-08-09 16:36:25 -0700

    commit tree 5437122f574fac814fcc0f5edfb992af4f2d49e3

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,18 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState = { friends: [] };
+
+export function manageFriends(state = initialState, action) {
+  switch(action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, {
+        name: action.payload.name,
+        hometown: action.payload.hometown,
+        id: action.payload.id
+      }]}
+
+    case "friends/remove":
+      return { friends: state.friends.filter(friend => friend.id !== action.payload) }
+
+    default: 
+      return state;
+  }
 }
